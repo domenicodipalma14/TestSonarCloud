19412b487d9c7cde0148ad3bf6e8f480b070d1ae
domenicodipalma14 <72913888+domenicodipalma14@users.noreply.github.com>
2020-10-29 21:02:38 +0100

    aggiorno Gioca.java

--- src/pezzo/Gioca.java
+++ src/pezzo/Gioca.java
@@ -4,7 +4,7 @@ public class Gioca {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scacchiera s;
-		int position [] = new int[2];
+		int position [];
 		s = new Scacchiera();
 		s.stampa();
 		
